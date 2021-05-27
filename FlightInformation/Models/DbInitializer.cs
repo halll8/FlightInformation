04397e5c0d504d946762182ec5b3458f8d902b4d
Lucas Hall <halll8@wwu.edu>
2021-05-27 14:23:28 -0700

    Commented out test data

--- FlightInformation/Models/DbInitializer.cs
+++ FlightInformation/Models/DbInitializer.cs
@@ -5,61 +5,62 @@ namespace FlightInformation.Data
 {
     public static class DbInitializer
     {
+        // Uncomment the code in Initialize to seed the database with some test data
         public static void Initialize(FlightInfoContext context)
         {
-            context.Database.EnsureCreated();
+            //context.Database.EnsureCreated();
 
             
-            if (context.Flights.Any())
-            {
-                return; 
-            }
+            //if (context.Flights.Any())
+            //{
+            //    return; 
+            //}
 
-            var flights = new Flight[]
-            {
-            new Flight{FlightNumber=1,AirLine="Alaska Airlines"},
-            new Flight{FlightNumber=2,AirLine="Delta Airlines"},
-            new Flight{FlightNumber=3,AirLine="Air Canada"}
-            };
-            foreach (Flight f in flights)
-            {
-                context.Flights.Add(f);
-            }
-            context.SaveChanges();
+            //var flights = new Flight[]
+            //{
+            //new Flight{FlightNumber=1,AirLine="Alaska Airlines"},
+            //new Flight{FlightNumber=2,AirLine="Delta Airlines"},
+            //new Flight{FlightNumber=3,AirLine="Air Canada"}
+            //};
+            //foreach (Flight f in flights)
+            //{
+            //    context.Flights.Add(f);
+            //}
+            //context.SaveChanges();
 
-            var passengers = new Passenger[]
-            {
-            new Passenger{PassengerID=15,FirstName="Lucas",LastName="Hall"},
-            new Passenger{PassengerID=42,FirstName="Esther",LastName="Chong"},
-            new Passenger{PassengerID=41,FirstName="Danny",LastName="Hassler"}
-            };
-            foreach (Passenger p in passengers)
-            {
-                context.Passengers.Add(p);
-            }
-            context.SaveChanges();
+            //var passengers = new Passenger[]
+            //{
+            //new Passenger{PassengerID=15,FirstName="Lucas",LastName="Hall"},
+            //new Passenger{PassengerID=42,FirstName="Esther",LastName="Chong"},
+            //new Passenger{PassengerID=41,FirstName="Danny",LastName="Hassler"}
+            //};
+            //foreach (Passenger p in passengers)
+            //{
+            //    context.Passengers.Add(p);
+            //}
+            //context.SaveChanges();
 
-            var tickets = new Ticket[]
-            {
-                new Ticket {
-                    FlightID = flights.Single(f => f.AirLine == "Alaska Airlines").ID,
-                    PassengerID = passengers.Single(p => p.LastName == "Hall").PassengerID 
-                },
-                new Ticket {
-                    FlightID = flights.Single(f => f.AirLine == "Delta Airlines").ID,
-                    PassengerID = passengers.Single(p => p.LastName == "Chong").PassengerID
-                },
-                new Ticket {
-                    FlightID = flights.Single(f => f.AirLine == "Air Canada").ID,
-                    PassengerID = passengers.Single(p => p.LastName == "Hassler").PassengerID
-                },
+            //var tickets = new Ticket[]
+            //{
+            //    new Ticket {
+            //        FlightID = flights.Single(f => f.AirLine == "Alaska Airlines").ID,
+            //        PassengerID = passengers.Single(p => p.LastName == "Hall").PassengerID 
+            //    },
+            //    new Ticket {
+            //        FlightID = flights.Single(f => f.AirLine == "Delta Airlines").ID,
+            //        PassengerID = passengers.Single(p => p.LastName == "Chong").PassengerID
+            //    },
+            //    new Ticket {
+            //        FlightID = flights.Single(f => f.AirLine == "Air Canada").ID,
+            //        PassengerID = passengers.Single(p => p.LastName == "Hassler").PassengerID
+            //    },
 
-            };
-            foreach (Ticket t in tickets)
-            {
-                context.Tickets.Add(t);
-            }
-            context.SaveChanges();
+            //};
+            //foreach (Ticket t in tickets)
+            //{
+            //    context.Tickets.Add(t);
+            //}
+            //context.SaveChanges();
 
         }
     }
